c960bfedc1997fc2434f699dc1b911ca08c4b11b
Victor Alex <victoralexsander5200@gmail.com>
2022-01-12 02:26:38 -0300

    cross filter config

--- src/main/java/com/example/guustobackend/controller/AuthController.java
+++ src/main/java/com/example/guustobackend/controller/AuthController.java
@@ -1,10 +1,12 @@
 package com.example.guustobackend.controller;
 
+import com.example.guustobackend.domain.dto.TokenDTO;
 import com.example.guustobackend.domain.model.AuthRequest;
 import com.example.guustobackend.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,16 +22,16 @@ public class AuthController {
 
 
     @PostMapping("/auth")
-    public String generateToken(@RequestBody AuthRequest authRequest) throws Exception {
+    public TokenDTO generateToken(@RequestBody AuthRequest authRequest) throws Exception {
         try {
             authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
                             authRequest.getUserName(),
                             authRequest.getPassword()));
         } catch (Exception ex) {
-            throw new Exception("inavalid username/password");
+            throw new Exception("invalid username/password");
         }
 
-        return jwtUtil.generateToken(authRequest.getUserName());
+        return TokenDTO.builder().token(jwtUtil.generateToken(authRequest.getUserName())).build();
     }
 
 }
